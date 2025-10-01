1a0308d4d51edc9e2021cf7f7cc78d8692ab0d44
Moustiikk <thomas.pizziniasmar@gmail.com>
2025-10-01 15:11:41 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
-#include "dependencies/vector.h"
 #include <stdio.h>
 #include <math.h>
 #include <SDL3/SDL.h>
 #include <stdbool.h>
+#include "dependencies/vector.h"
+#include "dependencies/fish.h"
+
